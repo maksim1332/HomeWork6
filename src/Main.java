8b276e04488e41b1693520e83c23fd538c7ccd9d
USER <mas9n9-mas9n91332@yandex.ru>
2024-09-23 04:29:05 +0500

    Completed all the tasks!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!");
         //tesk1
         System.out.println("Tesk 1--------------------------------------------------------------------------------");
         for (int i = 1; i <= 10; i++) {
