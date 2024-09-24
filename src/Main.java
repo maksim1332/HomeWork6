a87292acb77f09bca4de33302c040b29eb994ce5
USER <mas9n9-mas9n91332@yandex.ru>
2024-09-25 01:38:06 +0500

    Completed all the tasks1!

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,60 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
-        //tesk1
-        System.out.println("Tesk 1--------------------------------------------------------------------------------");
+        //task1
+        System.out.println("Task 1--------------------------------------------------------------------------------");
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
-        //tesk 2
-        System.out.println("Tesk 2--------------------------------------------------------------------------------");
+        //task 2
+        System.out.println("Task 2--------------------------------------------------------------------------------");
         for (int i = 10; i >= 1; i--) {
             System.out.println(i);
         }
-        //tesk 3
-        System.out.println("Tesk 3--------------------------------------------------------------------------------");
+        //task 3
+        System.out.println("Task 3--------------------------------------------------------------------------------");
         for (int i = 0; i < 17; i = i + 2) {
             System.out.println(i);
         }
-        //tesk 4
-        System.out.println("Tesk 4--------------------------------------------------------------------------------");
+        //task 4
+        System.out.println("Task 4--------------------------------------------------------------------------------");
         for (int i = 10; i >= -10; i = i - 1) {
             System.out.println(i);
         }
-        //tesk5
-        System.out.println("Tesk 5--------------------------------------------------------------------------------");
+        //task5
+        System.out.println("Task 5--------------------------------------------------------------------------------");
         for (int i = 1904; i <= 2096; i = i + 4) {
             System.out.println(i + "год является високосным");
         }
-        //tesk 6
-        System.out.println("Tesk 6--------------------------------------------------------------------------------");
+        //task 6
+        System.out.println("Task 6--------------------------------------------------------------------------------");
         for (int i = 7; i < 100; i = i + 7) {
             System.out.println(i);
         }
-        //tesk 7
-        System.out.println("Tesk 7--------------------------------------------------------------------------------");
+        //task 7
+        System.out.println("Task 7--------------------------------------------------------------------------------");
         for (int i = 1; i < 1000; i = i * 2) {
             System.out.println(i);
         }
-        //tesk 8
-        System.out.println("Tesk 8--------------------------------------------------------------------------------");
+        //task 8
+        System.out.println("Task 8--------------------------------------------------------------------------------");
         int sum = 29000;
         int savings = 0;
         for (int i = 0; i < 12; i++) {
             savings = savings + sum;
             System.out.println("Месяц " + i + " сумма накоплений равна " + savings + " рублей");
         }
-        //tesk 9
-        System.out.println("Tesk 9--------------------------------------------------------------------------------");
-        int sum9 = 29000;
-        int savings9 = 0;
-        for (int i = 0; i < 12; i++) {
-            savings9 = savings9 + savings9 / 100;
-            savings9 = savings9 + sum9;
-            System.out.println("Месяц " + i + " сумма накоплений равна " + savings9 + " рублей");
+        //task 9
+        System.out.println("Task 9--------------------------------------------------------------------------------");
+        double sum9 = 29000;
+        double  savings9 = 0;
+        double rate = 1.01;
+        for (int i = 1; i <= 12; i++) {
+            savings9 = savings9 * rate + sum9;
+            System.out.printf("Месяц %d, сумма накоплений равна %.2f рублей\n", i,  savings9);
         }
-        //tesk 10
-        System.out.println("Tesk 10--------------------------------------------------------------------------------");
+        //task 10
+        System.out.println("Task 10--------------------------------------------------------------------------------");
         int namber = 2;
         for (int i = 1; i <= 10; i++) {
             namber = 2 * i;
