f75100c41a25f91a66e59752288d3ffb99e0b8eb
USER <mas9n9-mas9n91332@yandex.ru>
2024-09-27 01:52:57 +0500

    Completed all the tasks1+!

--- src/Main.java
+++ src/Main.java
@@ -63,7 +63,7 @@ public class Main {
             System.out.println("2*" + i + "=" + namber);
         }
         //+task
-        System.out.println("+Task-----------------------------------------------------------");
+        System.out.println("+Task------------------------------------------------------------");
         //=
         System.out.println("Hello world!!");
         System.out.println("Hello world!!!");
